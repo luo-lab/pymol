709ab6b1fd1881c9a5da1fcdda97172696c19823
vyd2 <33358662+vyd2@users.noreply.github.com>
2017-12-13 15:47:31 -0800

    Update mutate_residues.py

--- mutate_residues.py
+++ mutate_residues.py
@@ -14,13 +14,16 @@ file = ["YP.pdb"] # name of your pdb file
 px = [4,5,7,9,20,23,27,33]
 lst = map(str,px) # make them into a string bc chimera doesn't accept floats below
 
+# loop through residues
 for i in lst: 
+	
+	# loop through the pdb file
 	for fn in file: 
 		replyobj.status("Processing " + fn) # show what file we're working on
 		rc("open " + fn) # need these plus signs to insert variable
 		rc("swapaa ala :"+i+".a") #swapaa command 'swaps' residue e.g. 4 into an alanine 
 		rc("write #0 ~/cloudv/WW/YP-WW/YP_"+i+"A.pdb") # write out pdbfile to location
-		rc("close all") #close session
+		rc("close all") #close models, doesn't close session
 
 # uncomment to to close chimera session
-# rc(close all)
+# rc("close session")
